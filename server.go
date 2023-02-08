d308703b05d8f0f4eda3864f6f9b2e8da70421d1
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-02-08 22:07:48 +0100

    updated port for deployment to elastic beanstalk

---
+++ server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"golang-gin-project/controller"
+	"golang-gin-project/middlewares"
+	"golang-gin-project/service"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+
+	setupLogOutput()
+	server := gin.New()
+
+	server.Static("/css", "./templates/csss")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	server.Use(
+		gin.Recovery(),
+		middlewares.Logger(),
+		// middlewares.BasicAuth(),
+		gindump.Dump(),
+	)
+
+	apiRoutes := server.Group("/api")
+	{
+
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+
+			err := videoController.Save(ctx)
+			if err != nil {
+
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid"})
+			}
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "5000"
+	}
+
+	server.Run(":" + port)
+}
